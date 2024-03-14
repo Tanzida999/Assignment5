88b0e79b03c41edadfc075760451962a35e4f782
Tanzida Nowshin <nowshinsara555@gmail.com>
2024-03-14 14:27:38 +0600

    Oriented

--- scripts/p-ticket.js
+++ scripts/p-ticket.js
@@ -2,3 +2,7 @@ function play(){
     hideElementById('start');
     showElementById('main');
 }
+
+const currentSeat = getTextElementValueById('seats-left');
+const updatedSeat = currentSeat - 1;
+setTextElementValueByID('seats-left', updatedSeat);
