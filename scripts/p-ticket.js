6fc037fd72e762fa1d94f94394d1d588612a4bd3
Tanzida Nowshin <nowshinsara555@gmail.com>
2024-03-15 13:25:46 +0600

    Updated

--- scripts/p-ticket.js
+++ scripts/p-ticket.js
@@ -3,6 +3,18 @@ function play(){
     showElementById('main');
 }
 
-const currentSeat = getTextElementValueById('seats-left');
-const updatedSeat = currentSeat - 1;
-setTextElementValueByID('seats-left', updatedSeat);
+// const currentSeat = getTextElementValueById('seats-left');
+// const updatedSeat = currentSeat - 1;
+// setTextElementValueByID('seats-left', updatedSeat);
+
+// const currentSeatNumber = document.getElementById('current-alphabet');
+// const currentAlphabet = currentAlphabetElement.innerText;       
+// const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+function handleKeyboardKeyUpEvent(){
+    const passengerPressed = event.key;
+    console.log('passenger pressed', passengerPressed);
+    if(passengerPressed ){
+        console.log('Passenger pressed', passengerPressed)
+    }
+    
+}
